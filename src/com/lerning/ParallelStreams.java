7e75e6bab1e11734bde7d9564749d5eccd35599d
Murillo Cesar Pinto Nogueria <murilloc@gmail.com>
2022-02-13 20:32:57 -0300

    Parallel streams

---
+++ src/com/lerning/ParallelStreams.java
@@ -0,0 +1,30 @@
+package com.lerning;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class ParallelStreams {
+
+    public static void main(String[] args) {
+        String[] wordsArray = {"hello", "java", "functional", "programming", "is", "cool"};
+        List<String> words = new ArrayList<>(Arrays.asList(wordsArray));
+
+        List<String> processedWords = words
+                .parallelStream()
+                .map((word) -> {
+                    System.out.printf("Uppercasing: %s%n", word);
+                    return word.toUpperCase();
+                })
+                .map((word) -> {
+                    System.out.printf("Adding exlcamation poit to %s%n", word);
+                    return word + '!';
+                })
+                .toList();
+
+        System.out.println(processedWords);
+
+    }
+}
